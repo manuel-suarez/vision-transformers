4524f0c212f2e46e2ccfcb3371cc9607c4b10fad
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-21 11:28:40 -0600

    Create patches

--- main.py
+++ main.py
@@ -25,3 +25,33 @@ train_data_batches = training_data.shuffle(buffer_size=40000).batch(128).prefetc
 valid_data_batches = validation_data.shuffle(buffer_size=10000).batch(32).prefetch(buffer_size=autotune)
 test_data_batches = test_data.shuffle(buffer_size=10000).batch(32).prefetch(buffer_size=autotune)
 
+##### generate patches
+class generate_patch(layers.Layer):
+    def __init__(self, patch_size):
+        super(generate_patch, self).__init__()
+        self.patch_size = patch_size
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(images=images,
+                                           sizes=[1, self.patch_size, self.patch_size, 1],
+                                           strides=[1, self.patch_size, self.patch_size, 1],
+                                           rates=[1, 1, 1, 1],
+                                           padding="VALID")
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+
+#############
+# visualize
+#############
+from itertools import islice, count
+
+train_iter_7im, train_iter_7label = next(islice(training_data, 7, None))
+train_iter_7im = tf.expand_dims(train_iter_7im, 0)
+train_iter_7label = train_iter_7label.numpy()
+patch_size=4
+
+generate_patch_layer = generate_patch(patch_size=patch_size)
+patches = generate_patch_layer(train_iter_7im)
+
+print('patch per image and patches shape: ', patches.shape[1], '\n', patches.shape)
